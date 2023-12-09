213aa2efc2eb748dc0cec061f8ced9649f7e7d3d
Daniel <druiz07@ucm.es>
2023-12-09 16:05:52 +0100

    Sigue faltando que los parametros se actualizen con escape pero todo lo demas guay

--- The Explorer SON/Assets/Scripts/PlayAmbience.cs
+++ The Explorer SON/Assets/Scripts/PlayAmbience.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
 
 //DUDAS: Por que se baja el sonido cuando se reproduce la musica ambiente 
 //Como podriamos cambiar los parametros de FMOD?
@@ -8,6 +9,8 @@ public class PlayAmbience : MonoBehaviour
 {
     // Start is called before the first frame update
     private bool isEscapePressed = false;
+    EventInstance music;
+    EventInstance ambience;
     void Awake()
     {
         StartCoroutine(PlayAmbienceAfterDelay());
@@ -15,15 +18,17 @@ public class PlayAmbience : MonoBehaviour
 
     IEnumerator PlayAmbienceAfterDelay()
     {
-        while (GameManager.Instance == null)
+        while (GameManager.Instance.audioManager == null || GameManager.Instance.fmodEvents == null)
         {
-            // Esperar hasta que GameManager esté disponible
             yield return null;
         }
 
-        // Ahora GameManager está disponible, podemos reproducir el sonido
-        GameManager.Instance.audioManager.PlayOneShotSound(GameManager.Instance.fmodEvents.GetEvent("Ambience"), transform.position);
-        GameManager.Instance.audioManager.PlayOneShotSound(GameManager.Instance.fmodEvents.GetEvent("Music"), transform.position);
+
+        music = GameManager.Instance.audioManager.CreateInstance(GameManager.Instance.fmodEvents.GetEvent("Music"));
+        ambience = GameManager.Instance.audioManager.CreateInstance(GameManager.Instance.fmodEvents.GetEvent("Ambience"));
+
+
+       
 
 
         GameManager.Instance.audioManager.SetParameter(GameManager.Instance.fmodEvents.GetEvent("Ambience"), "MusicIntensity",0.5f);
@@ -34,14 +39,18 @@ public class PlayAmbience : MonoBehaviour
 
         GameManager.Instance.audioManager.SetParameter(GameManager.Instance.fmodEvents.GetEvent("Music"), "MusicIntensity", 0.25f);
         GameManager.Instance.audioManager.SetParameter(GameManager.Instance.fmodEvents.GetEvent("Music"), "EqualisationLevel", 0.0f);
-       
+
+
+        // Ahora GameManager está disponible, podemos reproducir el sonido
+        music.start();
+        ambience.start();
     }
 
     // DUDA POR QUE ESTO NO ME ESTA HACIENDO NI CASO?
     void Update()
     {
         // Verificar si la tecla Escape ha sido presionada
-        if (Input.GetKeyDown(KeyCode.Delete))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             // Cambiar el valor del parámetro de ecualización al pulsar Escape
             isEscapePressed = !isEscapePressed;
